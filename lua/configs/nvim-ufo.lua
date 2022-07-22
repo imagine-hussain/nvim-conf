185c825223462228861bd9f9f158ff6bd0d48fe3
imagine-hussain <hussain.nwz000@gmail.com>
2022-07-22 17:44:32 +1000

    make ufo work

--- lua/configs/nvim-ufo.lua
+++ lua/configs/nvim-ufo.lua
@@ -1,6 +1,11 @@
 -- use {'kevinhwang91/nvim-ufo', requires = 'kevinhwang91/promise-async'}
 
 vim.o.foldcolumn = '1'
+vim.o.foldlevel = 99 -- feel free to decrease the value
+vim.o.foldenable = true
+
+vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
+vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)vim.o.foldcolumn = '1'
 vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
 vim.o.foldlevelstart = -1
 vim.o.foldenable = true
@@ -8,30 +13,6 @@ vim.o.foldenable = true
 -- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
 vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
 vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
-
--- option 1: coc.nvim as LSP client
--- use {'neoclide/coc.nvim', branch = 'master', run = 'yarn install --frozen-lockfile'}
--- require('ufo').setup()
---
-
--- option 2: nvim lsp as LSP client
--- tell the server the capability of foldingRange
--- nvim hasn't added foldingRange to default capabilities, users must add it manually
--- local capabilities = vim.lsp.protocol.make_client_capabilities()
--- capabilities.textDocument.foldingRange = {
---     dynamicRegistration = false,
---     lineFoldingOnly = true
--- }
--- local language_servers = {} -- like {'gopls', 'clangd'}
--- for _, ls in ipairs(language_servers) do
---     require('lspconfig')[ls].setup({
---         capabilities = capabilities,
---         other_fields = ...
---     })
--- end
--- require('ufo').setup()
---
-
 -- option 3: treesitter as a main provider instead
 local status_ok, ufo = pcall(require, "ufo")
 if status_ok then
@@ -60,3 +41,27 @@ end
 --         return ''
 --     end
 -- })
+
+-- option 1: coc.nvim as LSP client
+-- use {'neoclide/coc.nvim', branch = 'master', run = 'yarn install --frozen-lockfile'}
+-- require('ufo').setup()
+--
+
+-- option 2: nvim lsp as LSP client
+-- tell the server the capability of foldingRange
+-- nvim hasn't added foldingRange to default capabilities, users must add it manually
+-- local capabilities = vim.lsp.protocol.make_client_capabilities()
+-- capabilities.textDocument.foldingRange = {
+--     dynamicRegistration = false,
+--     lineFoldingOnly = true
+-- }
+-- local language_servers = {} -- like {'gopls', 'clangd'}
+-- for _, ls in ipairs(language_servers) do
+--     require('lspconfig')[ls].setup({
+--         capabilities = capabilities,
+--         other_fields = ...
+--     })
+-- end
+-- require('ufo').setup()
+--
+
