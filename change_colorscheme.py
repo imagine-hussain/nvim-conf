b9f9c6a9aba2ce500bce20c95ae3e623ca6cb63f
imagine-hussain <hussain.nwz000@gmail.com>
2022-10-17 20:54:35 +1100

    make clorscheme exclusive

--- change_colorscheme.py
+++ change_colorscheme.py
@@ -10,20 +10,44 @@ import sys
 
 config_root: str = os.path.dirname(os.path.abspath(__file__))
 colors_file = os.path.join(config_root, "lua", "configs", "colors.lua")
-print(colors_file)
-
-colorscheme = sys.argv[1].strip()
 
 def get_colorscheme() -> str:
-    colorscheme: str = sys.argv[1].strip()
+    try:
+        colorscheme: str = sys.argv[1].strip()
+    except IndexError:
+        exit_no_colorscheme()
+    except Exception as e:
+        print(e)
+        exit_no_colorscheme()
+
+    if not colorscheme:
+        exit_no_colorscheme()
+
     # Update aliases
     if colorscheme in ["gruvbox", "g", "gruv"]:
         colorscheme = "gruvbox-material"
-    if colorscheme in ["kanagawa", "kg", "k"]:
+    elif colorscheme in ["kanagawa", "kg", "k"]:
         colorscheme = "kanagawa"
 
+    else:
+        # Remove this to allow any colorscheme
+        print(f"""
+    Only allowing supported colorschemes;
+    To change this, edit {__file__} lines 33-35 or,
+    provide a supported colorscheme
+
+    You provided: {colorscheme}
+    """)
+        sys.exit(1)
+
+    print("Changing colorscheme to", colorscheme)
     return colorscheme
 
+
+def exit_no_colorscheme():
+    print("No colorscheme provided")
+    print("Correct usage: python3 change_colorscheme.py <colorscheme>")
+    sys.exit(1)
 
 def read_file(path: str) -> str:
     with open(path, "r") as f:
@@ -34,7 +58,10 @@ def write_file(path: str, content: str):
         f.write(content)
 
 def update_colorscheme(file: str, colorscheme: str):
-    return re.sub(r"^vim.cmd\[\[colorscheme .*\]\]\) *$", f"vim.cmd([[colorscheme {colorscheme}]])", file, flags=re.MULTILINE)
+    r: str = re.sub(r"^vim.cmd\[\[colorscheme .*\]\]\) *$", f"vim.cmd([[colorscheme {colorscheme}]])", file, flags=re.MULTILINE)
+    if r == file:
+        print("No changes made")
+    return r
 
 def main():
     colorscheme = get_colorscheme()
