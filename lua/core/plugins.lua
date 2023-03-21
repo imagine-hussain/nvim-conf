038e858a635ceb55f2e6b55305039038d5113818
imagine-hussain <hussain.nwz000@gmail.com>
2023-03-21 21:16:36 +1100

    comment.nvim setup

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -406,10 +406,10 @@ return {
   -- Commenting
   {
     "numToStr/Comment.nvim",
-    module = { "Comment", "Comment.api" },
-    keys = { "gc", "gb", "g<", "g>" },
-    config = function()
-      require "configs.Comment"
+    keys = { { "gc", mode = { "n", "v" } }, { "gb", mode = { "n", "v" } } },
+    opts = function()
+      local commentstring_avail, commentstring = pcall(require, "ts_context_commentstring.integrations.comment_nvim")
+      return commentstring_avail and commentstring and { pre_hook = commentstring.create_pre_hook() } or {}
     end,
   },
   --[[ ["SirVer/ultisnips"] = { ]]
